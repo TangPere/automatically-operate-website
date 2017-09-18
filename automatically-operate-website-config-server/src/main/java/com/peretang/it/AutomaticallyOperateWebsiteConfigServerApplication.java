d4c06acdf3fdaa0783eb84d669b676b5443ef34e
Hao Feng Pere Deng <pere.h.f.deng@noexternalmail.hsbc.com>
2017-09-18 15:47:27 +0800

    config server

---
+++ automatically-operate-website-config-server/src/main/java/com/peretang/it/AutomaticallyOperateWebsiteConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.peretang.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticallyOperateWebsiteConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticallyOperateWebsiteConfigServerApplication.class, args);
+	}
+}
---
+++ automatically-operate-website-config-server/src/main/java/com/peretang/it/controller/ConfigController.java
@@ -0,0 +1,25 @@
+/*
+ * COPYRIGHT. Pere Tang 2017. ALL RIGHTS RESERVED.
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer language
+ * in any way or for any other purposes whatsoever without the prior written
+ * consent of Pere Tang.
+ */
+package com.peretang.it.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Pere H F DENG
+ * @date 2017/8/4.
+ */
+@RestController("/config")
+public class ConfigController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ConfigController.class);
+
+
+}
